035b645b8d147b3cf823c2d89ce93838be048a11
sharond.lin@gmail.com <sharond.lin@gmail.com@c3ba3c7c-17bd-da4f-dc5f-f31b732ae7e9>
2013-09-04 07:49:56 +0000

    Recoloring layers in C#

--- trunk/LocallyLinearEmbedding/BaseCodeDLL/BaseCodeDLL.cpp
+++ trunk/LocallyLinearEmbedding/BaseCodeDLL/BaseCodeDLL.cpp
@@ -3,7 +3,7 @@
 BASECODEDLL_API void* __stdcall BCInit()
 {
     App *app = new App;
-    app->Init();
+    //app->Init();
     return app;
 }
 
@@ -36,10 +36,18 @@ BASECODEDLL_API BCBitmapInfo* __stdcall BCQueryBitmapByName(void *context, const
     return app.QueryBitmapByName(s);
 }
 
-BASECODEDLL_API BCLayers* __stdcall BCGetLayers(void *context, const char *s)
-{
-    if(context == NULL) return 0;
-    App &app = *(App*)context;
 
-	return app.GetLayers();
+BASECODEDLL_API BCLayers* __stdcall BCExtractLayers(void *context, BCBitmapInfo bitmap, const double* palette, int paletteSize)
+{
+	if (context == NULL) return 0;
+	App *app = (App*) context;
+
+	Vector<Vec3f> p;
+	int numColors = paletteSize;
+	double* data = (double*)palette;
+
+	for (int i=0; i<numColors; i++)
+		p.PushEnd(Vec3f((float)data[3*i], (float)data[3*i+1], (float)data[3*i+2]));
+	
+	return app ->ExtractLayers(bitmap, p);
 }
