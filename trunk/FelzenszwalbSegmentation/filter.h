ccdde87d45109375d257a216a8ebe7c865c46699
techmatt@gmail.com <techmatt@gmail.com@c3ba3c7c-17bd-da4f-dc5f-f31b732ae7e9>
2013-09-10 18:16:05 +0000

    Framework for constraints (not finished yet).

--- trunk/FelzenszwalbSegmentation/filter.h
+++ trunk/FelzenszwalbSegmentation/filter.h
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 /* normalize mask so it integrates to one */
 static void normalize(std::vector<float> &mask) {
-  int len = mask.size();
+  int len = (int)mask.size();
   float sum = 0;
   for (int i = 1; i < len; i++) {
     sum += fabs(mask[i]);
--- trunk/LocallyLinearEmbedding/BaseCodeDLL/BaseCodeDLL.cpp
+++ trunk/LocallyLinearEmbedding/BaseCodeDLL/BaseCodeDLL.cpp
@@ -38,7 +38,7 @@ BASECODEDLL_API BCBitmapInfo* __stdcall BCQueryBitmapByName(void *context, const
 }
 
 
-BASECODEDLL_API BCLayers* __stdcall BCExtractLayers(void *context, BCBitmapInfo bitmap, const double* palette, int paletteSize)
+BASECODEDLL_API BCLayers* __stdcall BCExtractLayers(void *context, BCBitmapInfo bitmap, const double* palette, int paletteSize, const char *constraints)
 {
 	if (context == NULL) return 0;
 	App *app = (App*) context;
@@ -50,7 +50,7 @@ BASECODEDLL_API BCLayers* __stdcall BCExtractLayers(void *context, BCBitmapInfo 
 	for (int i=0; i<numColors; i++)
 		p.PushEnd(Vec3f((float)data[3*i], (float)data[3*i+1], (float)data[3*i+2]));
 	
-	return app ->ExtractLayers(bitmap, p);
+	return app->ExtractLayers(bitmap, p, constraints);
 }
 
 BASECODEDLL_API BCBitmapInfo* __stdcall BCSegmentImage(void* context, BCBitmapInfo bitmap)
